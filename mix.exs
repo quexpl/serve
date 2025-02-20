20b118a3fdaa10d4dde3739674f8aeff74fb107e
Piotr Baj <piotr.baj@petabajt.net>
2025-02-21 00:12:37 +0100

    Require elixir >= 1.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Serve.MixProject do
     [
       app: :serve,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
